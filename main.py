1d6759821f2d0cd4f637ae2531ed9cd2ac47853f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-10 16:27:43 +0100

    ModifiedM main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-from flet import *
+import flet as ft
+from json import load
 
 
-def main(page: Page) -> None:
-    ...
-    gv = GridView(
+def main(page: ft.Page) -> None:
+    board = ft.GridView(
         expand=True,
-        max_extent=150,
+        max_extent=81,
         child_aspect_ratio=1,
-        col=8,
     )
-    page.add(gv)
 
-    for i in range(81):
-        gv.controls.append(
-            Container(
-                Text(f"Item {i}"),
-                alignment=alignment.center,
-                bgcolor=colors.GREEN if i % 2 == 0 else colors.WHITE,
+    page.add(board)
+
+    with open("assets/chess.json", "r") as f:
+        pieces: list[dict] = load(f)
+        for piece in pieces:
+            board.controls.append(
+                ft.Container(
+                    ft.Text(piece["piece"], size=18, weight=ft.font_weight.bold),
+                    width=80,
+                    height=80,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.GREEN
+                    if pieces.index(piece) % 2 == 0
+                    else ft.colors.WHITE,
+                )
             )
-        )
+
     page.update()
 
 
-app(
+ft.app(
     target=main,
-    view=AppView.FLET_APP,
+    view=ft.AppView.FLET_APP,
     assets_dir="./assets",
     use_color_emoji=True,
 )
