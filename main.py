c64a207b064748e0f600132a14bdf98cef4f0054
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-10 16:35:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from json import load
 
 
 def main(page: ft.Page) -> None:
+    page.window_resizable = False
+    page.
+    page.window_width = 800
+    page.window_height = 600
     board = ft.GridView(
         expand=True,
-        max_extent=81,
         child_aspect_ratio=1,
     )
 
@@ -16,9 +19,7 @@ def main(page: ft.Page) -> None:
         for piece in pieces:
             board.controls.append(
                 ft.Container(
-                    ft.Text(piece["piece"], size=18, weight=ft.font_weight.bold),
-                    width=80,
-                    height=80,
+                    ft.Text(piece["piece"], size=18, weight=ft.FontWeight.W_500),
                     alignment=ft.alignment.center,
                     bgcolor=ft.colors.GREEN
                     if pieces.index(piece) % 2 == 0
