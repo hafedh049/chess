af2db24f10e8fd7503251fac405224bb88894546
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-20 21:32:49 +0100

    ModifiedM main.py

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ def main(page: ft.Page) -> None:
     page.window_height = 600
     page.window_width = 600
 
+    painter: bool = True
+
+    for row in str(board).split("\n"):
+        new_row = row.split()
+
+        print(new_row)
+        row_widget : ft.Row = ft.Row()
+        for piece in new_row:
+
+            painter = not painter
+         page.add(
+                ft.Container(bgcolor=ft.colors.WHITE if painter else ft.colors.GREEN)
+            )
+
     page.update()
 
     print(board)
